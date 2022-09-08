f9756cd3d1cdc060b96b88a71ad9f4ce0e158576
MohitMaliFtechiz <mohit@gmail.com>
2022-09-08 12:22:06 +0530

    11.0.0 building
    Former-commit-id: e59fde73214378c890255e73ae46fc7be9a307d0

---
+++ android-libkiwixbuild/app/src/main/cpp/include/search_renderer.h
@@ -0,0 +1,135 @@
+/*
+ * Copyright 2011 Emmanuel Engelhart <kelson@kiwix.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU  General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ */
+
+#ifndef KIWIX_SEARCH_RENDERER_H
+#define KIWIX_SEARCH_RENDERER_H
+
+#include <string>
+#include <zim/search.h>
+#include "library.h"
+
+namespace kiwix
+{
+
+class Searcher;
+class NameMapper;
+/**
+ * The SearcherRenderer class is used to render a search result to a html page.
+ */
+class SearchRenderer
+{
+ public:
+  /**
+   * Construct a SearchRenderer from a Searcher.
+   *
+   * This method is now deprecated. Construct the renderer from a
+   * `zim::SearchResultSet`
+   *
+   * @param searcher The `Searcher` to render.
+   * @param mapper The `NameMapper` to use to do the rendering.
+   */
+  DEPRECATED SearchRenderer(Searcher* searcher, NameMapper* mapper);
+
+  /**
+   * Construct a SearchRenderer from a SearchResultSet.
+   *
+   * The constructed version of the SearchRenderer will not introduce
+   * the book name for each result. It is better to use the other constructor
+   * with a Library pointer to have a better html page.
+   *
+   * @param srs The `SearchResultSet` to render.
+   * @param mapper The `NameMapper` to use to do the rendering.
+   * @param start The start offset used for the srs.
+   * @param estimatedResultCount The estimatedResultCount of the whole search
+   */
+  SearchRenderer(zim::SearchResultSet srs, NameMapper* mapper,
+                 unsigned int start, unsigned int estimatedResultCount);
+
+  /**
+   * Construct a SearchRenderer from a SearchResultSet.
+   *
+   * @param srs The `SearchResultSet` to render.
+   * @param mapper The `NameMapper` to use to do the rendering.
+   * @param library The `Library` to use to look up book details for search results.
+   * @param start The start offset used for the srs.
+   * @param estimatedResultCount The estimatedResultCount of the whole search
+   */
+  SearchRenderer(zim::SearchResultSet srs, NameMapper* mapper, Library* library,
+                 unsigned int start, unsigned int estimatedResultCount);
+
+  ~SearchRenderer();
+
+  /**
+   * Set the search pattern used to do the search
+   */
+  void setSearchPattern(const std::string& pattern);
+
+  /**
+   * Set the querystring used to select books
+   */
+  void setSearchBookQuery(const std::string& bookQuery);
+
+  /**
+   * Set protocol prefix.
+   */
+  void setProtocolPrefix(const std::string& prefix);
+
+  /**
+   * Set search protocol prefix.
+   */
+  void setSearchProtocolPrefix(const std::string& prefix);
+
+  /**
+   * set result count per page
+   */
+  void setPageLength(unsigned int pageLength){
+    this->pageLength  = pageLength;
+  }
+
+  std::string renderTemplate(const std::string& tmpl_str);
+
+  /**
+   * Generate the html page with the resutls of the search.
+   */
+  std::string getHtml();
+
+    /**
+   * Generate the xml page with the resutls of the search.
+   */
+  std::string getXml();
+
+
+ protected:
+  std::string beautifyInteger(const unsigned int number);
+  zim::SearchResultSet m_srs;
+  NameMapper* mp_nameMapper;
+  Library* mp_library;
+  std::string searchBookQuery;
+  std::string searchPattern;
+  std::string protocolPrefix;
+  std::string searchProtocolPrefix;
+  unsigned int pageLength;
+  unsigned int estimatedResultCount;
+  unsigned int resultStart;
+};
+
+
+}
+
+#endif
---
+++ android-libkiwixbuild/app/src/main/cpp/include/zim/search_iterator.h
@@ -0,0 +1,79 @@
+/*
+ * Copyright (C) 2021 Maneesh P M <manu.pm55@gmail.com>
+ * Copyright (C) 2020 Matthieu Gautier <mgautier@kymeria.fr>
+ * Copyright (C) 2006 Tommi Maekitalo
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but
+ * is provided AS IS, WITHOUT ANY WARRANTY; without even the implied
+ * warranty of MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE, and
+ * NON-INFRINGEMENT.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+ *
+ */
+
+#ifndef ZIM_SEARCH_ITERATOR_H
+#define ZIM_SEARCH_ITERATOR_H
+
+#include <memory>
+#include <iterator>
+#include "entry.h"
+#include "archive.h"
+#include "uuid.h"
+
+namespace zim
+{
+class SearchResultSet;
+
+class SearchIterator : public std::iterator<std::bidirectional_iterator_tag, Entry>
+{
+    friend class zim::SearchResultSet;
+    public:
+        SearchIterator();
+        SearchIterator(const SearchIterator& it);
+        SearchIterator& operator=(const SearchIterator& it);
+        SearchIterator(SearchIterator&& it);
+        SearchIterator& operator=(SearchIterator&& it);
+        ~SearchIterator();
+
+        bool operator== (const SearchIterator& it) const;
+        bool operator!= (const SearchIterator& it) const;
+
+        SearchIterator& operator++();
+        SearchIterator operator++(int);
+        SearchIterator& operator--();
+        SearchIterator operator--(int);
+
+        std::string getPath() const;
+        std::string getTitle() const;
+        int getScore() const;
+        std::string getSnippet() const;
+        int getWordCount() const;
+        int getSize() const;
+        int getFileIndex() const;
+        Uuid getZimId() const;
+        reference operator*() const;
+        pointer operator->() const;
+
+#ifdef ZIM_PRIVATE
+        std::string getDbData() const;
+#endif
+
+    private:
+        struct InternalData;
+        std::unique_ptr<InternalData> internal;
+        SearchIterator(InternalData* internal_data);
+
+        bool isEnd() const;
+};
+
+} // namespace zim
+
+#endif // ZIM_SEARCH_ITERATOR_H
