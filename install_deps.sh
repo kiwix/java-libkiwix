7de75101fb4973a00603a354b52826328a3fafe4
Matthieu Gautier <mgautier@kymeria.fr>
2022-12-20 14:43:29 +0100

    Adapt README.md and change `build.sh` into `install_deps.sh`

--- install_deps.sh
+++ install_deps.sh
@@ -14,8 +14,6 @@ printf "${Green}Downloading libzim ${NC}\n"
 ./gradlew downloadLibzimSoAndHeaderFiles unzipLibzim
 printf  "\n${Green}Done! ${NC}\n"
 
-hash -r
-
 printf "${Green}Coping libzim header and so files ${NC}\n"
 ./gradlew checkCurrentLibzimDate copyLibzimHeaderFiles copyLibzimAndroidArm copyLibzimAndroidArm64 copyLibzimAndroidx86 copyLibzimAndroidx86_64 copyLibzimLinux_x86_64 renameLibzimSoFile
 printf  "\n${Green}Down! ${NC}\n"
@@ -24,14 +22,6 @@ printf "${Green}Downloading libkiwix ${NC}\n"
 ./gradlew downloadLibkiwixSoAndHeaderFiles unzipLibkiwix
 printf  "\n${Green}Done! ${NC}\n"
 
-hash -r
-
 printf "${Green}Coping libkiwix header and so files ${NC}\n"
 ./gradlew checkCurrentLibkiwixDate copyLibkiwixHeaderFiles copyLibkiwixAndroidArm copyLibkiwixAndroidArm64 copyLibkiwixAndroidx86 copyLibkiwixAndroidx86_64 copyLibkiwixLinux_x86_64 renameLibkiwixSoFile
 printf  "\n${Green}Done! ${NC}\n"
-
-printf "${Green}Generating header files from java wrapper files ${NC}\n"
-./gradlew generateHeaderFilesFromJavaWrapper
-printf  "\n${Green}Done! ${NC}\n"
-
-hash -r
