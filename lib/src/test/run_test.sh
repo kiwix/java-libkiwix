bc93a5deed5d903035e431b86a9a6fe19a8a50b9
MohitMaliFtechiz <futuretechoworld.mohitmali@gmail.com>
2023-03-28 18:50:48 +0530

    Renamed compile_and_run_test.sh to run_test.sh as now this script is only for running test cases, removed unnecessary check for directory in run_test.sh

--- lib/src/test/run_test.sh
+++ lib/src/test/run_test.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-# This script compiles and runs the unit test to test the java wrapper.
+# This script runs the unit test to test the java wrapper.
 # This is not integrated in meson because ... this is not so easy.
 
 die()
@@ -9,25 +9,8 @@ die()
   exit 1
 }
 
-# Copy generated .so file to build directory to run test cases
-cd ../../../
-./gradlew copyBuildKiwixSoFile
-cd lib/src/test
-
-KIWIX_LIB_JAR=$1
-if [ -z $KIWIX_LIB_JAR ]
-then
-  die "You must give the path to the kiwixlib.jar as first argument"
-fi
-
-KIWIX_LIB_DIR=$2
-if [ -z $KIWIX_LIB_DIR ]
-then
-  die "You must give the path to directory containing libkiwix.so as second argument"
-fi
-
-KIWIX_LIB_JAR=$(readlink -f "$KIWIX_LIB_JAR")
-KIWIX_LIB_DIR=$(readlink -f "$KIWIX_LIB_DIR")
+KIWIX_LIB_JAR=$(readlink -f "$1")
+KIWIX_LIB_DIR=$(readlink -f "$2")
 TEST_SOURCE_DIR=$(dirname "$(readlink -f $0)")
 
 cd "$TEST_SOURCE_DIR"
